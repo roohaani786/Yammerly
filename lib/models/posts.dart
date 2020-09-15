4679d299159f1255262a164dccb66e7e6eda57be
Aman Khan <amankanaa@gmail.com>
2020-09-15 19:54:05 +0530

    Merge branch 'develop' into issue8
    # Conflicts:
    #	lib/camera/camera_screen.dart
    #	lib/camera/preview_screen.dart
    #	pubspec.lock

---
+++ lib/models/posts.dart
@@ -0,0 +1,34 @@
+class Posts {
+
+  String description;
+  String email;
+  String url;
+  String likes;
+  String timestamp;
+
+
+
+  Posts({this.email, this.url, this.description,this.timestamp,this.likes});
+
+//  Map toMap(Posts posts) {
+//    var data = Map<String, dynamic>();
+//    data['email'] = posts.email;
+//    data['url'] = posts.url;
+//    data['description'] = posts.description;
+//    data['timestamp'] = posts.timestamp;
+//    data['likes'] = posts.likes;
+//
+//    return data;
+//  }
+
+//  Posts.fromMap(Map<String, dynamic> mapData) {
+//
+//    this.email = mapData['email'];
+//    this.url = mapData['url'];
+//    this.description = mapData['description'];
+//    this.timestamp = mapData['timestamp'];
+//    this.likes = mapData['likes'];
+//
+//  }
+}
+
--- lib/views/tabs/feeds.dart
+++ lib/views/tabs/feeds.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:techstagram/models/posts.dart';
 import 'package:techstagram/models/user.dart';
+import 'package:techstagram/resources/auth.dart';
 import 'package:techstagram/services/database.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -21,28 +25,95 @@ class FeedsPage extends StatefulWidget {
 
 class _FeedsPageState extends State<FeedsPage> {
 
+  bool isLoading = true;
+  bool isEditable = false;
+  String loadingMessage = "Loading Profile Data";
+  TextEditingController emailController,urlController,descriptionController,
+  timestampController,likesController;
+  List<Posts> posts;
+
+  Map<String, dynamic> _profile;
+  bool _loading = false;
+
+  DocumentSnapshot docSnap;
+  FirebaseUser currUser;
+
+  ScrollController scrollController = new ScrollController();
+  Posts currentpost;
+
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+
+    super.initState();
+    // Subscriptions are created here
+    authService.profile.listen((state) => setState(() => _profile = state));
+
+    authService.loading.listen((state) => setState(() => _loading = state));
+    fetchPosts();
+  }
+
   Stream<QuerySnapshot> postsStream;
   final timelineReference = Firestore.instance.collection('posts');
-  ScrollController scrollController = new ScrollController();
-  Wiggle currentWiggle;
 
-  retrieveTimeline() async {
-    DatabaseService().getPosts().then((val) {
+  fetchPosts() async {
+
+    DatabaseService().getPosts().then((val){
       setState(() {
         postsStream = val;
       });
     });
   }
-
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: Container(
-        color: Colors.red,
-      ),
-    );
+      key: _scaffoldKey,
+      body: StreamBuilder(
+        stream: postsStream,
+        builder: (context, snapshot) {
+          return snapshot.hasData
+              ? ListView.builder(
+              controller: scrollController,
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) {
+                String email = snapshot.data.documents[index]['email'];
+                String description =
+                snapshot.data.documents[index]['description'];
+                Timestamp timestamp =
+                snapshot.data.documents[index]['timestamp'];
+                String url = snapshot.data.documents[index]['url'];
+                String postId = snapshot.data.documents[index]['postId'];
+                int likes = snapshot.data.documents[index]['likes'];
+
+                print(email);
+//                for (int i = 0; i < posts.length; i++) {
+//                  if (posts[i].email == email) {
+//                    currentpost = posts[i];
+//                  }
+//                }
+                return Container(
+                  height: 150.0,
+                  width: 150.0,
+                  child: Image.network(url),
+                );
+
+//                return FeedTile(
+//                  wiggle: currentpost,
+//                  wiggles: posts,
+//                  description: description,
+//                  timestamp: timestamp,
+//                  url: url,
+//                  postId: postId,
+//                  likes: likes,
+//                );
+              })
+              : Container();
+
+        },
+        ),
+      );
   }
 }
 
