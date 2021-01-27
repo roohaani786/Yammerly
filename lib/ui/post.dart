b7a6a997b82686337d2ad5fe41a0f5b3c3538e7b
shadaab88 <abdulshadaab4646@gmail.com>
2021-01-27 16:29:14 +0530

    lo bhai chai pe ke dekhte hai

--- lib/ui/post.dart
+++ lib/ui/post.dart
@@ -244,7 +244,7 @@ class _postPageState extends State<postPage> {
         time = "${diff.inHours} ${diff.inHours == 1 ? "hour" : "hours"} ago";
       }
 
-      else if (diff.inSeconds <= 0) {
+      else if (diff.inSeconds == 0 || diff.inSeconds < 60) {
         time = "just now";
       }
 
