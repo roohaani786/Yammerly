680011e919477b671822e8c25745ae2537ab7fe3
shadaab <abdulshadaab4646@gmail.com>
2021-04-09 16:46:59 +0530

    time

--- lib/ComeraV/Camera.dart
+++ lib/ComeraV/Camera.dart
@@ -69,7 +69,30 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
   double _currentScale = 1.0;
   double _baseScale = 1.0;
   List<CameraDescription> _cameras;
+  final Dependencies dependencies = new Dependencies();
+  final Stopwatch stopwatch = new Stopwatch();
+  final List<ValueChanged<ElapsedTime>> timerListeners = <ValueChanged<ElapsedTime>>[];
 
+
+  void leftButtonPressed() {
+    setState(() {
+      if (dependencies.stopwatch.isRunning) {
+        print("${dependencies.stopwatch.elapsedMilliseconds}");
+      } else {
+        dependencies.stopwatch.reset();
+      }
+    });
+  }
+
+  void rightButtonPressed() {
+    setState(() {
+      if (dependencies.stopwatch.isRunning) {
+        dependencies.stopwatch.stop();
+      } else {
+        dependencies.stopwatch.start();
+      }
+    });
+  }
   // Counting pointers (number of user fingers on screen)
   int _pointers = 0;
 
@@ -677,7 +700,7 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
       height: 100,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        mainAxisSize: MainAxisSize.max,
+        //mainAxisSize: MainAxisSize.max,
         children: <Widget>[
 
           IconButton(
@@ -748,9 +771,6 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
           //   ),
           // ),
           //
-
-
-
           (!_isRecordingMode)?CircleAvatar(
             backgroundColor: Colors.white,
             radius: 28.0,
@@ -763,39 +783,54 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
                   ? onTakePictureButtonPressed
                   : null,
             ),
-          ):Row(
+          ):Column(
             children: [
-
-              //the comment code is button for video playing
-              // IconButton(
-              //   icon: const Icon(Icons.videocam),
-              //   color: Colors.white,
-              //   onPressed: controller != null &&
-              //       controller.value.isInitialized &&
-              //       !controller.value.isRecordingVideo
-              //       ? onVideoRecordButtonPressed
-              //       : null,
-              // ),
-
-              IconButton(
-                icon: controller != null && controller.value.isRecordingPaused
-                    ? Icon(Icons.play_arrow)
-                    : Icon(Icons.pause),
-                color: Colors.white,
-                onPressed: controller != null && controller.value.isInitialized && controller.value.isRecordingVideo
-                    ? (controller != null && controller.value.isRecordingPaused
-                    ? onResumeButtonPressed
-                    : onPauseButtonPressed)
-                    : null,
-              ),
-              IconButton(
-                icon: const Icon(Icons.stop),
-                color: Colors.red,
-                onPressed: controller != null &&
-                    controller.value.isInitialized &&
-                    controller.value.isRecordingVideo
-                    ? onStopButtonPressed
-                    : null,
+              Container(
+                height: 20.0,
+                  child: TimerText(dependencies: dependencies)),
+              Row(
+                children: [
+                  //the comment code is button for video playing
+                  // IconButton(
+                  //   icon: const Icon(Icons.videocam),
+                  //   color: Colors.white,
+                  //   onPressed: controller != null &&
+                  //       controller.value.isInitialized &&
+                  //       !controller.value.isRecordingVideo
+                  //       ? onVideoRecordButtonPressed
+                  //       : null,
+                  // ),
+                  // GestureDetector(
+                  //   onTap: () {
+                  //     rightButtonPressed();
+                  //   },
+                  //   child: IconButton(
+                  //     icon: controller != null && controller.value.isRecordingPaused
+                  //         ? Icon(Icons.play_arrow)
+                  //         : Icon(Icons.pause),
+                  //     color: Colors.white,
+                  //     onPressed: controller != null && controller.value.isInitialized && controller.value.isRecordingVideo
+                  //         ? (controller != null && controller.value.isRecordingPaused
+                  //         ? onResumeButtonPressed
+                  //         : onPauseButtonPressed)
+                  //         : null,
+                  //   ),
+                  // ),
+                  GestureDetector(
+                    onTap: (){
+                      rightButtonPressed();
+                    },
+                    child: IconButton(
+                      icon: const Icon(Icons.stop),
+                      color: Colors.red,
+                      onPressed: controller != null &&
+                          controller.value.isInitialized &&
+                          controller.value.isRecordingVideo
+                          ? onStopButtonPressed
+                          : null,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
@@ -824,8 +859,9 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
                     !controller.value.isRecordingVideo
                     ? onVideoRecordButtonPressed()
                     : print("nahi gaya");
+                rightButtonPressed();
 
-                Timer(Duration(seconds: 30), () {
+                Timer(Duration(seconds: 300), () {
                   print("Yeah, this line is printed after 3 second");
                   controller != null &&
                       controller.value.isInitialized &&
@@ -1222,3 +1258,159 @@ class CameraApp extends StatelessWidget {
 //   }
 //   runApp(CameraApp());
 // }
+
+class ElapsedTime {
+  final int hundreds;
+  final int seconds;
+  final int minutes;
+
+  ElapsedTime({
+    this.hundreds,
+    this.seconds,
+    this.minutes,
+  });
+}
+
+class Dependencies {
+
+  final List<ValueChanged<ElapsedTime>> timerListeners = <ValueChanged<ElapsedTime>>[];
+  final TextStyle textStyle = const TextStyle(color: Colors.white,fontSize: 15.0, fontFamily: "Bebas Neue");
+  final Stopwatch stopwatch = new Stopwatch();
+  final int timerMillisecondsRefreshRate = 30;
+}
+
+class TimerText extends StatefulWidget {
+  TimerText({this.dependencies});
+  final Dependencies dependencies;
+
+  TimerTextState createState() => new TimerTextState(dependencies: dependencies);
+}
+
+class TimerTextState extends State<TimerText> {
+  TimerTextState({this.dependencies});
+  final Dependencies dependencies;
+  Timer timer;
+  int milliseconds;
+
+  @override
+  void initState() {
+    timer = new Timer.periodic(new Duration(milliseconds: dependencies.timerMillisecondsRefreshRate), callback);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    timer = null;
+    super.dispose();
+  }
+
+  void callback(Timer timer) {
+    if (milliseconds != dependencies.stopwatch.elapsedMilliseconds) {
+      milliseconds = dependencies.stopwatch.elapsedMilliseconds;
+      final int hundreds = (milliseconds / 10).truncate();
+      final int seconds = (hundreds / 100).truncate();
+      final int minutes = (seconds / 60).truncate();
+      final ElapsedTime elapsedTime = new ElapsedTime(
+        hundreds: hundreds,
+        seconds: seconds,
+        minutes: minutes,
+      );
+      for (final listener in dependencies.timerListeners) {
+        listener(elapsedTime);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new RepaintBoundary(
+          child: new SizedBox(
+            height: 20.0,
+            child: new MinutesAndSeconds(dependencies: dependencies),
+          ),
+        ),
+        new RepaintBoundary(
+          child: new SizedBox(
+            height: 20.0,
+            child: new Hundreds(dependencies: dependencies),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MinutesAndSeconds extends StatefulWidget {
+  MinutesAndSeconds({this.dependencies});
+  final Dependencies dependencies;
+
+  MinutesAndSecondsState createState() => new MinutesAndSecondsState(dependencies: dependencies);
+}
+
+class MinutesAndSecondsState extends State<MinutesAndSeconds> {
+  MinutesAndSecondsState({this.dependencies});
+  final Dependencies dependencies;
+
+  int minutes = 0;
+  int seconds = 0;
+
+  @override
+  void initState() {
+    dependencies.timerListeners.add(onTick);
+    super.initState();
+  }
+
+  void onTick(ElapsedTime elapsed) {
+    if (elapsed.minutes != minutes || elapsed.seconds != seconds) {
+      setState(() {
+        minutes = elapsed.minutes;
+        seconds = elapsed.seconds;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String minutesStr = (minutes % 60).toString().padLeft(2, '0');
+    String secondsStr = (seconds % 60).toString().padLeft(2, '0');
+    return new Text('$minutesStr:$secondsStr.', style: dependencies.textStyle);
+  }
+}
+
+class Hundreds extends StatefulWidget {
+  Hundreds({this.dependencies});
+  final Dependencies dependencies;
+
+  HundredsState createState() => new HundredsState(dependencies: dependencies);
+}
+
+class HundredsState extends State<Hundreds> {
+  HundredsState({this.dependencies});
+  final Dependencies dependencies;
+
+  int hundreds = 0;
+
+  @override
+  void initState() {
+    dependencies.timerListeners.add(onTick);
+    super.initState();
+  }
+
+  void onTick(ElapsedTime elapsed) {
+    if (elapsed.hundreds != hundreds) {
+      setState(() {
+        hundreds = elapsed.hundreds;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String hundredsStr = (hundreds % 100).toString().padLeft(2, '0');
+    return new Text(hundredsStr, style: dependencies.textStyle);
+  }
+}
