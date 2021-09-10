be8d1c670e73b7e2cf6141bcdd717144a5854a5b
shadaab <abdulshadaab4646@gmail.com>
2021-09-11 00:05:17 +0530

    done with all changes ready for release

--- lib/ComeraV/Camera.dart
+++ lib/ComeraV/Camera.dart
@@ -6,7 +6,7 @@
 
 import 'dart:async';
 import 'dart:io';
-import 'package:get/get.dart';
+// import 'package:get/get.dart';
 import 'package:camera/camera.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
